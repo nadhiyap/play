390d5959e8d0bd18a731eda3e4cb07d6e011d30e
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-03-17 10:51:53 +0530

    rec.py

---
+++ rec.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number"))
+b=int(input("enter another number"))
+if(a>b):
+    if(b+b<a):
+        print(no)
+    else:
+        print("yes")
+elif(a<b):
+    if(a+a<b):
+        print("no")
+    else:
+        print("yes")
+else:
+    print("no")
+
+
+      
