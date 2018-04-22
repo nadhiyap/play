21c78c21af42470d04a41785b777ef44e8349977
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-22 16:59:11 +0530

    csum3.py

---
+++ csum3.py
@@ -0,0 +1,18 @@
+n1=int(input("enter n"))
+l=[]
+c=0
+for i in range(n1):
+    a=int(input())
+    l.append(a)
+l.sort()
+s=int(len(l)/2)
+for i in range(s,n1):
+    if i==n1-1:
+        c=c+l[i]
+        break
+    else:
+        if l[i]+1==l[i+1]:
+           c=c+l[i]
+        else:
+            pass
+print(c)
